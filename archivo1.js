39a8c95e0a237f23cac45ed54f61e2bed8847ed3
LucasCambon <Lucambon08@gmail.com>
2022-03-31 22:44:18 -0300

    Contador hasta 20

--- archivo1.js
+++ archivo1.js
@@ -1,1 +1,9 @@
 console.log("Hola! Soy el archivo 1");
+
+console.log("--------- COMENZAMOS A CONTAR ---------")
+
+for (let i=0; i<=20; i++) {
+    console.log(i)
+}
+
+console.log("--------- TERMINAMOS DE CONTAR ---------")
